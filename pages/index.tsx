645a78b406eb8bed94ba6cd87f1e98c2f07fc90f
TechsavvYash <mittal.yash12@hotmail.com>
2022-01-18 21:10:01 +0530

    started with the profile page
    Signed-off-by: TechsavvYash <mittal.yash12@hotmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import GradientLayout from '../components/gradientLayout'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className={styles.container}>
-      Home
-    </div>
+    <GradientLayout color="green" subtitle="profile" title ="Yash Mittal" description ="15 public playlists" roundImage
+    image={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyzEWAgI0lRb2AKHZ6noIb6iMoAKYYO2-0xjyxtWVxKBh0SiCXc_wrustmY11CYC3RTSU&usqp=CAU"}>
+      <div>HOME PAGE</div>Home page
+    </GradientLayout>
   )
 }
+
+export default Home;
+
+
+
