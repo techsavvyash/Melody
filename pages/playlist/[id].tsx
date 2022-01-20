8520dec11783c5bdbc6b22c83e4efe71b1237bd6
TechsavvYash <mittal.yash12@hotmail.com>
2022-01-20 22:28:09 +0530

    started the playlists page
    Signed-off-by: TechsavvYash <mittal.yash12@hotmail.com>

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,10 +1,34 @@
 import { validateToken } from "../../lib/auth"
 import prisma from "../../lib/prisma";
+import GradientLayout from "../../components/gradientLayout";
 
+const getBGColor = (id) => {
+  const colors = [
+    'red',
+    'green',
+    'blue',
+    'orange',
+    'purple',
+    'gray',
+    'teal',
+    'yellow'
+  ]
+
+  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)]
+}
 
 const Playlist = ({playlist}) => {
+  const color = getBGColor(playlist.id) ;
     return (
+      <GradientLayout color = {color}
+      roundImage={false}
+      title={playlist.name}
+      subtitle="playlist"
+      description= {`${playlist.songs.length} songs`}
+      image = {`https://picsum.photos/400?random=${playlist.id}`}
+      >
         <div>{playlist.name}</div>
+      </GradientLayout>
     )
 }
 
