39fac014fef52b5ec819c63f2d2f7327da48fcbb
TechsavvYash <mittal.yash12@hotmail.com>
2022-03-04 16:37:36 +0530

    changes
    Signed-off-by: TechsavvYash <mittal.yash12@hotmail.com>

--- components/player.tsx
+++ components/player.tsx
@@ -1,226 +1,220 @@
 import {
-    ButtonGroup,
-    Box,
-    IconButton,
-    RangeSlider,
-    RangeSliderFilledTrack,
-    RangeSliderTrack,
-    RangeSliderThumb,
-    Center,
-    Flex,
-    Text
-} from "@chakra-ui/react"
+  ButtonGroup,
+  Box,
+  IconButton,
+  RangeSlider,
+  RangeSliderFilledTrack,
+  RangeSliderTrack,
+  RangeSliderThumb,
+  Center,
+  Flex,
+  Text,
+} from "@chakra-ui/react";
 
-import ReactHowler from 'react-howler'
-import { useEffect, useState, useRef } from "react"
+import ReactHowler from "react-howler";
+import { useEffect, useState, useRef } from "react";
 import {
-    MdShuffle,
-    MdSkipPrevious,
-    MdSkipNext,
-    MdOutlinePlayCircleFilled,
-    MdOutlinePauseCircleFilled,
-    MdOutlineRepeat
-} from 'react-icons/md'
+  MdShuffle,
+  MdSkipPrevious,
+  MdSkipNext,
+  MdOutlinePlayCircleFilled,
+  MdOutlinePauseCircleFilled,
+  MdOutlineRepeat,
+} from "react-icons/md";
 
-import { useStoreActions } from "easy-peasy"
-import { formatTime } from "../lib/formatter"
+import { useStoreActions } from "easy-peasy";
+import { formatTime } from "../lib/formatter";
 
-const Player = ({songs, activeSong}) => {
-    
-    const [playing, setPLaying] = useState(true)
-    const [index, setIndex] = useState(
-        songs.findIndex((s) => s.id === activeSong.id)
-    )
-    const [seek, setSeek] = useState(0.0)
-    const [isSeeking, setIsSeeking] = useState(false)
-    const [repeat, setRepeat] = useState(false)
-    const [shuffle, setShuffle] = useState(false)
-    const [duration, setDuration] = useState(0.0)
-    const soundRef = useRef(null)
-    const repeatRef = useRef(repeat) //a reference does not fall pray to closures
-    const setActiveSong = useStoreActions((state: any) => state.changeActiveSong)
-    //learn about requestAnimationFrame
-    //mostly everytime we use requestAnimationFrame its going to be recursive
-    useEffect(() => {
-        let timerId
+const Player = ({ songs, activeSong }) => {
+  const [playing, setPLaying] = useState(true);
+  const [index, setIndex] = useState(
+    songs.findIndex((s) => s.id === activeSong.id)
+  );
+  const [seek, setSeek] = useState(0.0);
+  const [isSeeking, setIsSeeking] = useState(false);
+  const [repeat, setRepeat] = useState(false);
+  const [shuffle, setShuffle] = useState(false);
+  const [duration, setDuration] = useState(0.0);
+  const soundRef = useRef(null);
+  const repeatRef = useRef(repeat); // a reference does not fall pray to closures
+  const setActiveSong = useStoreActions((state: any) => state.changeActiveSong);
+  // learn about requestAnimationFrame
+  // mostly everytime we use requestAnimationFrame its going to be recursive
+  useEffect(() => {
+    let timerId;
 
-        if(playing && !isSeeking) {
-            const f = () => {
-                setSeek(soundRef.current.seek())
-                timerId = requestAnimationFrame(f)
-            }
+    if (playing && !isSeeking) {
+      const f = () => {
+        setSeek(soundRef.current.seek());
+        timerId = requestAnimationFrame(f);
+      };
 
-            timerId = requestAnimationFrame(f)
-            return () => cancelAnimationFrame(timerId)
+      timerId = requestAnimationFrame(f);
+      return () => cancelAnimationFrame(timerId);
+    }
+
+    cancelAnimationFrame(timerId);
+  }, [playing, isSeeking]);
+
+  useEffect(() => {
+    setActiveSong(songs[index]);
+  }, [index, setActiveSong, songs]);
+
+  useEffect(() => {
+    repeatRef.current = repeat;
+  }, [repeat]);
+
+  const setPlayState = (value) => {
+    setPLaying(value);
+  };
+
+  const onShuffle = () => {
+    setShuffle((state) => !state);
+  };
+
+  const onRepeat = () => {
+    setRepeat((state) => !state);
+  };
+
+  const prevSong = () => {
+    setIndex((state) => {
+      return state ? state - 1 : songs.length - 1;
+    });
+  };
+
+  const nextSong = () => {
+    setIndex((state) => {
+      if (shuffle) {
+        const next = Math.floor(Math.random() * songs.length);
+        if (next === state) {
+          return nextSong();
         }
 
-        cancelAnimationFrame(timerId)
-    }, [playing, isSeeking])
+        return next;
+      }
+      return state === songs.length - 1 ? 0 : state + 1;
+    });
+  };
 
-    useEffect(() => {
-        setActiveSong(songs[index])
-    }, [index, setActiveSong, songs])
-
-    useEffect(() => {
-        repeatRef.current = repeat
-    }, [repeat])
-
-    const setPlayState = (value) => {
-        setPLaying(value) 
+  const onEnd = () => {
+    if (repeatRef) {
+      setSeek(0); // for UI
+      soundRef.current.seek(0); // make the song go back to start
+    } else {
+      nextSong();
     }
+  };
 
-    const onShuffle = () => {
-        setShuffle((state) => !state) 
-    }
+  const onLoad = () => {
+    const songDuration = soundRef.current.duration();
+    setDuration(songDuration);
+  };
 
-    const onRepeat = () => {
-        setRepeat((state) => !state)
-    }
+  const onSeek = (e) => {
+    setSeek(parseFloat(e[0]));
+    soundRef.current.seek(e[0]);
+  };
 
-    const prevSong = () => {
-        setIndex((state) => {
-            return state ? state - 1: songs.length - 1
-        })
-    }
+  return (
+    <Box>
+      <Box>
+        <ReactHowler
+          playing={playing}
+          src={activeSong?.url}
+          ref={soundRef}
+          onLoad={onLoad}
+          onEnd={onEnd}
+        />
+      </Box>
+      <Center color="gray.600">
+        <ButtonGroup>
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="shuffle"
+            fontSize="24px"
+            color={shuffle ? "white" : "gray.600"}
+            icon={<MdShuffle />}
+            onClick={onShuffle}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="shuffle"
+            fontSize="24px"
+            icon={<MdSkipPrevious />}
+            onClick={prevSong}
+          />
+          {playing ? (
+            <IconButton
+              outline="none"
+              variant="link"
+              aria-label="shuffle"
+              fontSize="40px"
+              icon={<MdOutlinePauseCircleFilled />}
+              onClick={() => setPlayState(false)}
+            />
+          ) : (
+            <IconButton
+              outline="none"
+              variant="link"
+              aria-label="shuffle"
+              fontSize="40px"
+              icon={<MdOutlinePlayCircleFilled />}
+              onClick={() => setPlayState(true)}
+            />
+          )}
 
-    const nextSong = () => {
-        setIndex((state) => {
-            if(shuffle) {
-                const next = Math.floor(Math.random() * songs.length)
-                if(next === state) {
-                    return nextSong() 
-                }
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="shuffle"
+            fontSize="24px"
+            icon={<MdSkipNext />}
+            onClick={nextSong}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="shuffle"
+            fontSize="24px"
+            color={repeat ? "white" : "gray.600"}
+            icon={<MdOutlineRepeat />}
+            onClick={onRepeat}
+          />
+        </ButtonGroup>
+      </Center>
 
-                return next
-                
-            } else {
-                return state === songs.length -1 ? 0 : state + 1
-            }
-        })
-    }
+      <Box color="gray.600">
+        <Flex justify="center" align="center">
+          <Box width="10%">
+            <Text fontSize="xs"> {formatTime(seek)} </Text>
+          </Box>
+          <Box width="80%">
+            <RangeSlider
+              aria-label={["min", "max"]}
+              steps={0.1}
+              min={0}
+              max={duration ? (duration.toFixed(2) as unknown as number) : 0}
+              onChange={onSeek}
+              value={[seek]}
+              onChangeStart={() => setIsSeeking(true)}
+              onChangeEnd={() => setIsSeeking(false)}
+              id="player-range"
+            >
+              <RangeSliderTrack bg="gray.800">
+                <RangeSliderFilledTrack bg="gray.600" />
+              </RangeSliderTrack>
+              <RangeSliderThumb index={0} />
+            </RangeSlider>
+          </Box>
+          <Box width="10%" textAlign="right">
+            <Text fontSize="xs"> {formatTime(duration)} </Text>
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
 
-    const onEnd = () => {
-        if(repeatRef) {
-            setSeek(0) //for UI
-            soundRef.current.seek(0) // make the song go back to start
-        } else {
-            nextSong() 
-        }
-    }
-
-    const onLoad = () => {
-        const songDuration = soundRef.current.duration()
-        setDuration(songDuration)
-    }
-
-    const onSeek = (e) => {
-        setSeek(parseFloat(e[0]))
-        soundRef.current.seek(e[0])
-    }
-
-    return (
-        <Box>
-            <Box>
-                <ReactHowler 
-                    playing={playing}
-                    src ={activeSong?.url}
-                    ref = {soundRef}
-                    onLoad = {onLoad}
-                    onEnd = {onEnd}
-                />
-            </Box>
-            <Center color="gray.600">
-                <ButtonGroup>
-                    <IconButton
-                        outline ="none"
-                        variant="link"
-                        aria-label="shuffle"
-                        fontSize="24px"
-                        color = {shuffle ? "white" : "gray.600"}
-                        icon={<MdShuffle />} 
-                        onClick={onShuffle}
-                    />
-                    <IconButton
-                        outline ="none"
-                        variant="link"
-                        aria-label="shuffle"
-                        fontSize="24px"
-                        icon={<MdSkipPrevious />} 
-                        onClick={prevSong}
-                    />
-                    {playing 
-                    ? (
-                        <IconButton
-                            outline ="none"
-                            variant="link"
-                            aria-label="shuffle"
-                            fontSize="40px"
-                            icon={<MdOutlinePauseCircleFilled />}
-                            onClick = {()=> setPlayState(false)} 
-                        />
-                    ) : (
-                        <IconButton
-                            outline ="none"
-                            variant="link"
-                            aria-label="shuffle"
-                            fontSize="40px"
-                            icon={<MdOutlinePlayCircleFilled />} 
-                            onClick={() => setPlayState(true)}
-                        />)
-                    }
-                    
-                    
-                    <IconButton
-                        outline ="none"
-                        variant="link"
-                        aria-label="shuffle"
-                        fontSize="24px"
-                        icon={<MdSkipNext />} 
-                        onClick={nextSong}
-
-                    />
-                    <IconButton
-                        outline ="none"
-                        variant="link"
-                        aria-label="shuffle"
-                        fontSize="24px"
-                        color = {repeat ? "white" : "gray.600"}
-                        icon={<MdOutlineRepeat />} 
-                        onClick={onRepeat}
-                    />
-                </ButtonGroup>
-            </Center>
-
-            <Box color = "gray.600" >
-                <Flex justify="center" align="center">
-                    <Box width = "10%">
-                        <Text fontSize="xs"> {formatTime(seek)} </Text>
-                    </Box>
-                    <Box width="80%">
-                        <RangeSlider 
-                            aria-label={['min', 'max']}
-                            steps={0.1}
-                            min={0}
-                            max= {duration ? (duration.toFixed(2) as unknown as number) :  0}
-                            onChange = {onSeek}
-                            value={[seek]}
-                            onChangeStart={() => setIsSeeking(true)}
-                            onChangeEnd ={() => setIsSeeking(false)}
-                            id="player-range"
-                        >
-                            <RangeSliderTrack bg="gray.800">
-                                <RangeSliderFilledTrack bg = "gray.600" />
-                            </RangeSliderTrack>
-                            <RangeSliderThumb index={0} />
-                        </RangeSlider>
-                    </Box>
-                    <Box width="10%" textAlign="right">
-                        <Text fontSize="xs"> {formatTime(duration)} </Text>
-                    </Box>
-                </Flex>
-            </Box>
-        </Box>
-    )
-}
-
-export default Player
+export default Player;
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,40 +1,40 @@
-import jwt from 'jsonwebtoken';
-import prisma from './prisma';
-import { NextApiRequest, NextApiResponse } from 'next'
+import jwt from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "./prisma";
 
 export const validateRoute = (handler) => {
-    return async (req: NextApiRequest, res: NextApiResponse) => {
-        const token = req.cookies.TRAX_ACCESS_TOKEN
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const token = req.cookies.TRAX_ACCESS_TOKEN;
 
-        if(token) {
-            let user
+    if (token) {
+      let user;
 
-            try {
-                const {id} = jwt.verify(token, 'hello')
-                user = prisma.user.findUnique({
-                    where: {id},
-                })
+      try {
+        const { id } = jwt.verify(token, "hello");
+        user = prisma.user.findUnique({
+          where: { id },
+        });
 
-                if(!user) {
-                    throw new Error('Not real user')
-                }
-            } catch(e) {
-                res.status(401)
-                res.json({
-                    error: "Not Authorised"
-                })
-                return 
-            }
+        if (!user) {
+          throw new Error("Not real user");
+        }
+      } catch (e) {
+        res.status(401);
+        res.json({
+          error: "Not Authorised",
+        });
+        return;
+      }
 
-            return handler(req, res, user) 
-        } 
-
-        res.status(401)
-        res.json({error: 'Not AUthorised'})
+      return handler(req, res, user);
     }
-}
+
+    res.status(401);
+    res.json({ error: "Not AUthorised" });
+  };
+};
 
 export const validateToken = (token) => {
-    const user = jwt.verify(token, 'hello')
-    return user
-  }
+  const user = jwt.verify(token, "hello");
+  return user;
+};
