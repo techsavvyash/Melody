45eaa88f7fe33a0b814732ea645ac0dd4f378720
TechsavvYash <mittal.yash12@hotmail.com>
2022-03-04 16:39:42 +0530

    changes
    Signed-off-by: TechsavvYash <mittal.yash12@hotmail.com>

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -1,49 +1,49 @@
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import cookie from 'cookie'
-import prisma from '../../lib/prisma'
-import { NextApiRequest, NextApiResponse } from 'next'
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../lib/prisma";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const salt = bcrypt.genSaltSync() ;
-    const {email, password} = req.body ;
+  const salt = bcrypt.genSaltSync();
+  const { email, password } = req.body;
 
-    let user ;
+  let user;
 
-    try {
-        user = await prisma.user.create({
-            data: {
-                firstName: "Yash",
-                lastName: "Mittal",
-                email: email,
-                password: bcrypt.hashSync(password, salt) 
-            }
-        })
-    } catch(err) {
-        res.status(401)
-        res.json({error: 'User already exists!'})
-    }
-    console.log('user', user)
-    const token = jwt.sign(
-    { 
-        email: user.email,
-        id: user.id,
-        time: Date.now(),
+  try {
+    user = await prisma.user.create({
+      data: {
+        firstName: "Yash",
+        lastName: "Mittal",
+        email: email,
+        password: bcrypt.hashSync(password, salt),
+      },
+    });
+  } catch (err) {
+    res.status(401);
+    res.json({ error: "User already exists!" });
+  }
+  console.log("user", user);
+  const token = jwt.sign(
+    {
+      email: user.email,
+      id: user.id,
+      time: Date.now(),
     },
-    'hello',
-    {expiresIn: '8h'}
-    )
+    "hello",
+    { expiresIn: "8h" }
+  );
 
-    res.setHeader(
-        'Set-Cookie', 
-        cookie.serialize('TRAX_ACCESS_TOKEN', token, {
-            httpOnly: true,
-            maxAge: 8 *60*60,
-            path: '/',
-            sameSite: 'lax',
-            secure: process.env.NODE_ENV === 'production',
-        })
-    )
+  res.setHeader(
+    "Set-Cookie",
+    cookie.serialize("TRAX_ACCESS_TOKEN", token, {
+      httpOnly: true,
+      maxAge: 8 * 60 * 60,
+      path: "/",
+      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+    })
+  );
 
-    res.json(user) 
-}
+  res.json(user);
+};
