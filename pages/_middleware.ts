3c2f059ea71de77b14ca24ffae737d6fb99adb36
TechsavvYash <mittal.yash12@hotmail.com>
2022-07-17 21:21:45 +0530

    added sample.env
    Signed-off-by: TechsavvYash <mittal.yash12@hotmail.com>

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 const signedinPages = ["/", "/playlist", "/library"];
 // it is a "edge function" ==> they work in web worker environment
 export default function middleware(req) {
-	if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
+  if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
     const token = req.cookies.TRAX_ACCESS_TOKEN;
 
     if (!token) {
